0b7812a83c3f5f14624bd4a3a764bbc20c0683ee
Connor Jennings <cojennin@gmail.com>
2012-12-14 20:46:00 -0500

    Modifying how the dom content is appended. Currently appending content, need to append dom object.

--- slideshow.php
+++ slideshow.php
@@ -12,24 +12,44 @@ require('/var/www/main/wp-content/themes/hatchet-main/header.php');
         </div>
         <div id="wchs-picture-wrapper" class="left">
             <div id="wchs-picture-container">
-                <div class="wchs-picture">
+                <div id="wchs-picture">
                     <img src="./imgs/temp_src_material/img_1.jpg" class="shadow relative-img" />
-                    <div id="left-nav" class="transparent"> </div>
-                    <img id="left-nav-img" class="not-transparent" src="./imgs/left-arrow-ss.png" />
                 </div>
+                <div id="left-nav" class="transparent"> </div>
+                <img id="left-nav-img" class="not-transparent" src="./imgs/left-arrow-ss-light.png" />
+                <div id="right-nav" class="transparent"> </div>
+                <img id="right-nav-img" class="not-transparent" src="./imgs/right-arrow-ss-light.png" />
             </div>
             <ul id="wchs-navigator">
-                <li><a href="#">Prev</a></li>
-                <li class="active"><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li><a href="#">Next</a></li>
+                <li id="wchs-nav-prev"><a href="#">Prev</a></li>
+                <li><a class="wchs-active" href="#">1</a></li>
+                <li><a class="wchs-nav-num" href="#">2</a></li>
+                <li><a class="wchs-nav-num" href="#">3</a></li>
+                <li><a class="wchs-nav-num" href="#">4</a></li>
+                <li><a class="wchs-nav-num" href="#">5</a></li>
+                <li><a class="wchs-nav-num" href="#">6</a></li>
+                <li><a class="wchs-nav-num" href="#">7</a></li>
+                <li><a class="wchs-nav-num" href="#">8</a></li>
+                <li><a class="wchs-nav-num" href="#">9</a></li>
+                <li><a class="wchs-nav-num" href="#">10</a></li>
+                <li><a class="wchs-nav-num" href="#">11</a></li>
+                <li><a class="wchs-nav-num" href="#">12</a></li>
+                <li id="wchs-nav-next"><a href="#">Next</a></li>
             </ul>
         </div>
         <div id="wchs-caption-container" class="left">
-
+            <div id="image-info">
+                <h4>Connor Jennings | Web Director</h4>
+                <p class="caption">This is a test caption for this image. What is this photo about? Not sure yet, content will go here.</p>
+            </div>
+            <div id="recent-slideshows">
+                <h3 class="small-light">Recent slideshows</h3>
+                <ul id="recent" class="slideshows">
+                    <li>Have you seen this cat?</li>
+                    <li>He is one cool cat.</li>
+                    <li>Last related slideshow test</li>
+                </ul>
+            </div>
         </div>
     </div>
 <?php
--- wch-slideshow.js
+++ wch-slideshow.js
@@ -1,34 +1,146 @@
 $(function(){
 	
-	/* Slideshow container view */
+	//Model for a single slide
+	//Contains the information about the slide:
+	//byline, caption, img src
+	var wchsSingleSlide = Backbone.Model.extend({
+		defaults: {
+			img: null,
+			byline: null,
+			caption: null,	
+		}
+	});
 
-	var wchSlideshowContainer = Backbone.View.extend({
+	//Holds all the slides together. Contains information 
+	//about the current slide, next slide, previous slide, total
+	//amt of slides. Should notify the navigation view when
+	//it has been updated
+	var wchsSlideCollection = Backbone.Collection.extend({
+		model: wchsSingleSlide,
+		defaults: {
+			current: null,
+			next: null,
+			prev: null,
+			total: null,
+		}
+	});
+
+
+	//Create slideshow wrapper. This view
+	//will wrap most of the functionality of the slideshow as well
+	//as fill in the handlbar markup correctly
+	var wchsSlideshowContainer = Backbone.View.extend({
 		
-		id: 'wch-ss-container',
-		//For the Hatchet, capture content so everything can be inserted into it
-		//vis-a-vis the wch-ss-container element, which will act as our slideshow
-		//container
-		el: $('#content'),
+		//Wrapper id
+		id:"wchs-picture-container",
 
+		//Need to parse through the handlebar content
+		initialize: function(options){
+			//Initialize variables for use later in function
+			this.wchs_width = options.w;
+			this.wchs_height = options.h;
+			this.wchs_slides = options.collection;
+			//Wrap the template container object in jQuery, use later
+			//on in parsing handlebar data
+			this.wchs_template_container = $('#'+options.template_container);
+
+			//Begin rendering slideshow container
+			this.render();
+		},
 
 		template: function(context){
-			//We want to return a template
-			var wchs_source = '<div id="wchs-slideshow">{{title}}</div>'
-			var wchs_template = Handlebars.compile(wchs_source);
-			var wchs_html = wchs_template(context);
+			var wchs_template = Handlebars.compile(this.wchs_template_container.html());
+
+			//Figure out how to bind single slide backbone models to 
+			//handlebar registerHelper functions. 
+			var slides_html = ""
+			Handlebars.registerHelper('each', function(context, options){
+				_.each(context, function(single_slide){
+					//Bind a view to each of the slides we're generating with handlebars
+					//Return the html content of the view
+					slides_html += new Handlebars.SafeString(new wchsSingleSlide({parent_view: this.$el, model:single_slide}).render());
+				});
+				return slides_html;
+			});
+
+			//var wchs_html = wchs_template({slides: this.wchs_slides.models});
+			var wchs_html = wchs_template(
+				{
+					slideshow: {
+						slides: this.wchs_slides.models
+					}
+				}
+			);
+
+
 			return wchs_html;
 		},
 
 		render: function(){
+			//Need to begin by replacing {{sidebar}} with 
+			//slideshow container div
+			//Gonna find if this merges with a global object or doesn't like being places
+			//in a function with limietd scoping, away from compilation (but next to templating!)
+			var that = this;
+			//Handlebars should handle the looping, so just pass
+			//slide to the function
 
-			var context = {title: "Test2"};
-
-			var html = this.template(context);
-			$(this.el).append(html);
+			var html = this.template(this.wchs_slides);
+			(this.$el).height(this.wchs_height).width(this.wchs_width)
+				.css('background-color', '#333').html(html)
+				.insertAfter(this.wchs_template_container);
 		}
 	});
 
-	var genSlideshow = new wchSlideshowContainer();
-	//genSlideshow.render();
+	//Single slide view. Handle stuff like hovering over an image.
+	//Clicking on navigation might best be handled by the container,
+	//as it should have acting control over the functionality of that
+	//material. Images really don't have much functionality, merely acting
+	//as placeholders for data.
+	var wchsSingleSlide = Backbone.View.extend({
 
+		model: wchsSingleSlide,
+
+		className: 'wchs-single-slide',
+
+		events: {
+			"click": "slideSelected"
+		},
+
+		initialize: function(options){
+			//Keep track of parent view, will 
+			//be attaching to it in the render function
+			this.parent_view = options.parent_view;
+		},
+
+		render: function(){
+			var slide_html = "<div class='"+this.className+"'><img src='"+this.model.get("img")+"' /></div>";
+			$(this.el).append(slide_html);
+			return slide_html;
+		},
+
+		slideSelected: function(){
+			alert("test");
+		}	
+
+	});
+
+	//The object being passed will be processed by handlebars
+	//When looping through on the each statement, new singleSlide models will
+	//be generated for each slide
+	var wchsSlides = new wchsSlideCollection([
+			{img: "./imgs/temp_src_material/img_1.jpg", byline: "Connor J", caption: "Test caption"},
+			{img: "./imgs/temp_src_material/img_2.jpg", byline: "Not Connor J", caption: "Not a test caption"},
+			{img: "./imgs/temp_src_material/img_3.jpg", byline: "A Connor J", caption: "A test caption"}
+		]
+	);
+
+	//Accept width, height, name of handlbar template script id,
+	//collection of slides
+	var wchSlideshow = new wchsSlideshowContainer({
+		w: 600,
+		h: 373,
+		template_container: "wchs-template",
+		collection: wchsSlides
+	});
 });
