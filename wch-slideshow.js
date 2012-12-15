22a6b4ddc24695dd373cd8576adc60a38cfb67e5
Connor Jennings <cojennin@gmail.com>
2012-12-15 02:49:24 -0500

    Slideshow advances in one direction on click.

--- wch-slideshow.js
+++ wch-slideshow.js
@@ -5,6 +5,7 @@ $(function(){
 	//byline, caption, img src
 	var wchsSingleSlide = Backbone.Model.extend({
 		defaults: {
+			id: null,
 			img: null,
 			byline: null,
 			caption: null,	
@@ -17,12 +18,6 @@ $(function(){
 	//it has been updated
 	var wchsSlideCollection = Backbone.Collection.extend({
 		model: wchsSingleSlide,
-		defaults: {
-			current: null,
-			next: null,
-			prev: null,
-			total: null,
-		}
 	});
 
 
@@ -33,6 +28,7 @@ $(function(){
 		
 		//Wrapper id
 		id:"wchs-picture-container",
+		wchs_index: 0,
 
 		//Need to parse through the handlebar content
 		initialize: function(options){
@@ -40,30 +36,39 @@ $(function(){
 			this.wchs_width = options.w;
 			this.wchs_height = options.h;
 			this.wchs_slides = options.collection;
+			this.total_width = 0;
+
 			//Wrap the template container object in jQuery, use later
 			//on in parsing handlebar data
 			this.wchs_template_container = $('#'+options.template_container);
-
+			//When collection changes, re-render the view to accurately display
+			
+			//this.on("change", this.render(), this.wchs_slides);
 			//Begin rendering slideshow container
 			this.render();
+		},
+
+		events: {
+			"click": "rotateSlideForwardByOne"
 		},
 
 		template: function(context){
 			var wchs_template = Handlebars.compile(this.wchs_template_container.html());
 
 			//Figure out how to bind single slide backbone models to 
-			//handlebar registerHelper functions. 
-			var slides_html = ""
+			//handlebar registerHelper funccollectiotions. 
+			//Calling this in underscore references underscores this
+			this.$el.html("");
+			var container_this = this;
 			Handlebars.registerHelper('each', function(context, options){
-				_.each(context, function(single_slide){
+				_.each(context, function(single_slide, i){
 					//Bind a view to each of the slides we're generating with handlebars
 					//Return the html content of the view
-					slides_html += new Handlebars.SafeString(new wchsSingleSlide({parent_view: this.$el, model:single_slide}).render());
+					(container_this.$el).append(new wchsSingleSlide({model:single_slide, shift_by:container_this.wchs_index, parent_view:container_this}).render().el);
+					container_this.total_width += container_this.wchs_width;
 				});
-				return slides_html;
 			});
 
-			//var wchs_html = wchs_template({slides: this.wchs_slides.models});
 			var wchs_html = wchs_template(
 				{
 					slideshow: {
@@ -72,24 +77,30 @@ $(function(){
 				}
 			);
 
-
-			return wchs_html;
 		},
 
 		render: function(){
-			//Need to begin by replacing {{sidebar}} with 
-			//slideshow container div
-			//Gonna find if this merges with a global object or doesn't like being places
-			//in a function with limietd scoping, away from compilation (but next to templating!)
-			var that = this;
-			//Handlebars should handle the looping, so just pass
-			//slide to the function
+			//Render the entirety of the slideshow, take established width (ie: how many slides
+			//are in the slideshow)
+			//Run the template to generate single slides
+			//given handlebar markup
+			this.template(this.wchs_slides);
+			$('#wchs-slideshow-wrapper').html((this.$el).height(this.wchs_height).width(this.total_width));
+			//Need to explicity call delegateEvents or events won't rebind because appending
+			//html to wrapper, not explicity inserting into already created container
+			this.delegateEvents();
 
-			var html = this.template(this.wchs_slides);
-			(this.$el).height(this.wchs_height).width(this.wchs_width)
-				.css('background-color', '#333').html(html)
-				.insertAfter(this.wchs_template_container);
-		}
+			//Clean up and remove handlebar markup
+			this.wchs_template_container.remove();
+		},
+
+		rotateSlideForwardByOne: function(){
+			if(this.wchs_index < this.wchs_slides.length-1){
+				this.wchs_index++;
+				this.render();
+			}
+		},
+
 	});
 
 	//Single slide view. Handle stuff like hovering over an image.
@@ -101,37 +112,56 @@ $(function(){
 
 		model: wchsSingleSlide,
 
-		className: 'wchs-single-slide',
-
-		events: {
-			"click": "slideSelected"
-		},
+		className: 'wchs-single-slide-standard',
 
 		initialize: function(options){
-			//Keep track of parent view, will 
-			//be attaching to it in the render function
-			this.parent_view = options.parent_view;
+			this.shift_by = options.shift_by;
+			this.parent = options.parent_view;
+		},
+
+		/*events: {
+			"click": "slideSelected"
+		},*/
+
+		render: function(){
+			var slide_html = "<img src='"+this.model.get("img")+"' />";
+			$(this.el).css('left', (this.shift_by*this.parent.wchs_width)*-1)
+			$(this.el).append(slide_html);
+			return this;
+		},
+
+		/*slideSelected: function(){
+			alert("test");
+		}*/	
+
+	});
+
+	var wchsSlideNavView = Backbone.View.extend({
+
+		clasName: 'wchs-slide-nav',
+
+		intialize: function(options){
+			//Designate left or right arrow
+			this.id = options.id;
+		},
+
+		events: {
+			"hover": "showSlide",
+			"click": "moveOneSlide"
 		},
 
 		render: function(){
-			var slide_html = "<div class='"+this.className+"'><img src='"+this.model.get("img")+"' /></div>";
-			$(this.el).append(slide_html);
-			return slide_html;
-		},
-
-		slideSelected: function(){
-			alert("test");
-		}	
-
+			return this;
+		}
 	});
 
 	//The object being passed will be processed by handlebars
 	//When looping through on the each statement, new singleSlide models will
 	//be generated for each slide
 	var wchsSlides = new wchsSlideCollection([
-			{img: "./imgs/temp_src_material/img_1.jpg", byline: "Connor J", caption: "Test caption"},
-			{img: "./imgs/temp_src_material/img_2.jpg", byline: "Not Connor J", caption: "Not a test caption"},
-			{img: "./imgs/temp_src_material/img_3.jpg", byline: "A Connor J", caption: "A test caption"}
+			{id: 1, img: "./imgs/temp_src_material/img_1.jpg", byline: "Connor J", caption: "Test caption"},
+			{id: 2, img: "./imgs/temp_src_material/img_2.jpg", byline: "Not Connor J", caption: "Not a test caption"},
+			{id: 3, img: "./imgs/temp_src_material/img_3.jpg", byline: "A Connor J", caption: "A test caption"}
 		]
 	);
 
@@ -139,8 +169,9 @@ $(function(){
 	//collection of slides
 	var wchSlideshow = new wchsSlideshowContainer({
 		w: 600,
-		h: 373,
+		h: 400,
 		template_container: "wchs-template",
 		collection: wchsSlides
 	});
+
 });
